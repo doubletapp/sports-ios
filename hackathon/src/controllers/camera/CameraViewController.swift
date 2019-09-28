0a79073b624a33bc862055f9a5480af5b76ff06e
k4russ <k4russ@gmail.com>
2019-09-28 11:50:56 +0300

    Base for camera screen 2

--- hackathon/src/controllers/camera/CameraViewController.swift
+++ hackathon/src/controllers/camera/CameraViewController.swift
@@ -2,4 +2,9 @@ import Foundation
 import UIKit
 
 class CameraViewController: UIViewController {
+
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var galleryButton: UIButton!
+    @IBOutlet weak var switchCameraButton: UIButton!
 }
