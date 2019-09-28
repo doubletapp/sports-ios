13a4be19b04207141043728fe6bf2b22882b4193
k4russ <k4russ@gmail.com>
2019-09-29 01:17:34 +0300

    Mapping Highlights

--- hackathon/src/model/HighlightModel.swift
+++ hackathon/src/model/HighlightModel.swift
@@ -7,4 +7,25 @@ struct HighlightModel {
     let match: MatchModel
     let fragments: [FragmentModel]
     let events: [EventModel]
+
+    static func models(from json: [String: Any]) -> [HighlightModel] {
+        var models = [HighlightModel]()
+
+        (json["highlights"] as? [[String: Any]])?.forEach { modelJson in
+            models.append(HighlightModel.model(from: modelJson))
+        }
+
+        return models
+    }
+
+    static func model(from json: [String: Any]) -> HighlightModel {
+        return HighlightModel(
+            id: json["id"] as! Int, 
+            videoUrl: json["video_url"] as! String,
+            previewUrl: json["preview_url"] as! String, 
+            match: MatchModel.model(from: json["match"] as! [String: Any]),
+            fragments: [], //TODO
+            events: (json["events"] as? [[String: Any]])?.map { EventModel.model(from: $0) } ?? []
+        )
+    }
 }
